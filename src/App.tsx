2664872d8ec5c72dee3b3f7ed3432087152ac5ef
ABRAHAM-PC\Abraham <abraham1128@gmail.com>
2021-07-14 00:58:16 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import PerfilDesarrollador from './components/PerfilDesarrollador';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PerfilDesarrollador></PerfilDesarrollador>
+    </div>
+  );
+}
+
+export default App;
